4953f6c350b7ac908e460fabddf4becb6ba55138
Joseph Gao <gaoj@seas.upenn.edu>
2017-01-31 22:34:48 -0500

    finished

--- src/main.js
+++ src/main.js
@@ -5,9 +5,10 @@ const THREE = require('three'); // older modules are imported like this. You sho
 import Framework from './framework'
 
 var tick = 0;
+var damping = 0.1;
 var feathers = 100;
-var flap_motion = 100;
 var featherGeo;
+var wind_direction = 0;
 
 // bone stuff
 var bone_geometry = new THREE.Geometry();
@@ -16,7 +17,7 @@ var boneMaterial = new THREE.LineBasicMaterial( { color: 0xe3dac9, linewidth: 1 
 var bone = new THREE.Line(bone_geometry, boneMaterial);
 
 
-var base_color = 0x000000;
+var base_color = 0xFFDFF00;
 
 var feather_color = function() {
     this.color = 0;
@@ -38,15 +39,31 @@ var wind_direction = function() {
     this.wind_direction = 0;
 }
 
+var feather_size = function() {
+    this.feather_size = 1;
+}
+
+var flap_speed = function() {
+    this.flap_speed = 1;
+}
+
+var flap_range = function() {
+    this.flap_range = 1;
+}
+
 // defining our scales
 var wind_speed_scale = new wind_speed();
 var wing_curve_scale = new wing_curve();
 var feather_color_scale = new feather_color();
 var visible_feathers_scale = new visible_feathers();
 var wind_direction_scale = new wind_direction();
+var feather_size_scale = new feather_size();
+var feather_flap_speed = new flap_speed();
+var feather_flap_range = new flap_range();
 
 // called after the scene loads
 function onLoad(framework) {
+    console.log('hi');
     var scene = framework.scene;
     var camera = framework.camera;
     var renderer = framework.renderer;
@@ -74,9 +91,9 @@ function onLoad(framework) {
 
     scene.background = skymap;
 
-    curve.v0 = new THREE.Vector3(0, 0, 0);
-    curve.v1 = new THREE.Vector3(3, 0, 0);
-    curve.v2 = new THREE.Vector3(6, 0, 0);
+    curve.v0 = new THREE.Vector3(-3, 0, 0);
+    curve.v1 = new THREE.Vector3(0, 0, 0);
+    curve.v2 = new THREE.Vector3(3, 0, 0);
     for (var j = 0; j < feathers; j++) {
         bone_geometry.vertices.push(curve.getPoint(j / feathers));
     }
@@ -92,12 +109,6 @@ function onLoad(framework) {
         var featherMesh = new THREE.Mesh(featherGeo, lambertWhite);
         featherMesh.name = "feather";
         featherMesh.position.set(0.1, 0.3, 0);
-        //var clone = featherMesh.clone(true);
-        //clone.position.set(0.0, 0.0, 0.0);
-        //clone.rotateZ(-70);
-        // Y by PI/2
-        // Z by 180
-        //scene.add(featherMesh);
         for (var i = 0; i < bone_geometry.vertices.length; i++) {
             var vertex = bone_geometry.vertices[i];
             var feather = featherMesh.clone(true);
@@ -115,7 +126,6 @@ function onLoad(framework) {
     camera.position.set(0, 1, 5);
     camera.lookAt(new THREE.Vector3(0,0,0));
 
-    // scene.add(lambertCube);
     scene.add(directionalLight);
 
     // edit params and listen to changes like this
@@ -150,8 +160,9 @@ function onLoad(framework) {
 
     gui.add(wing_curve_scale, 'curve', -5, 5).onChange(function(newVal) {
         wing_curve_scale.curve = newVal;
-        curve.v1 = new THREE.Vector3(3, newVal, 0);
-        //curve.v2 = new THREE.Vector3(6, newVal, 0);
+        curve.v0 = new THREE.Vector3(-3, -1 * newVal, 0);
+        curve.v1 = new THREE.Vector3(0, newVal, 0);
+        curve.v2 = new THREE.Vector3(3, -1 * newVal, 0);
         scene.remove(bone);
         bone_geometry = new THREE.Geometry();
         for (var j = 0; j < feathers; j++) {
@@ -165,41 +176,84 @@ function onLoad(framework) {
             var feather = framework.scene.getObjectByName(i);
             feather.position.set(vertex.x, vertex.y, vertex.z);
         }
-
-        for (var i = 0; i < visible_feathers_scale.feather_count; i++) {
-            var feather = framework.scene.getObjectByName(i);
-            feather.traverse( function ( object ) { object.visible = true; });
-        }
-        for (var i = Math.round(visible_feathers_scale.feather_count); i < 100; i++) {
-            var feather = framework.scene.getObjectByName(i);
-            feather.traverse( function ( object ) { object.visible = false; });
-        }
     });
 
     gui.add(wind_direction_scale, 'wind_direction', 0, 1).onChange(function(newVal) {
+        if (wind_direction !== 0) {
+            for (var i = 0; i < feathers; i++) {
+                var feather = framework.scene.getObjectByName(i);
+                if (feather && newVal != 1) {
+                    feather.rotateY(0.03);
+                }
+            }
+            if (newVal === 0) {
+                wind_direction = 0;
+            }
+        }
+        else {
+            for (var i = 0; i < feathers; i++) {
+                var feather = framework.scene.getObjectByName(i);
+                if (feather && newVal != 0) {
+                    feather.rotateY(-0.03);
+                }
+            }
+            if (newVal === 1) {
+                wind_direction = 1;
+            }
+        }
         wind_direction_scale.wind_direction = newVal;
+    });
+
+    gui.add(feather_size_scale, 'feather_size', 1, 10).onChange(function(newVal) {
+        feather_size_scale.feather_size = newVal;
+    });
+
+    gui.add(feather_flap_speed, 'flap_speed', 1, 10).onChange(function(newVal) {
+        feather_flap_speed.flap_speed = newVal;
+    });
+
+    gui.add(feather_flap_range, 'flap_range', 1, 5).onChange(function(newVal) {
+        feather_flap_range.flap_range = newVal;
     });
 }
 
 // called on frame updates
 function onUpdate(framework) {
-    //var feather = framework.scene.getObjectByName("feather");
-    //if (feather !== undefined) {
-        // Simply flap wing
-        //feather.rotateZ(Math.sin(date.getTime() / 100) * 2 * Math.PI / 180);
-    //}
-    tick += 1;
+    tick += feather_flap_speed.flap_speed / 10;
+    var flap_range = feather_flap_range.flap_range;
+    var disp = flap_range * Math.sin(tick);
+
+    curve.v0 = new THREE.Vector3(-3, -1 * disp, 0);
+    curve.v1 = new THREE.Vector3(0, disp, 0);
+    curve.v2 = new THREE.Vector3(3, -1 * disp, 0);
+    framework.scene.remove(bone);
+    bone_geometry = new THREE.Geometry();
+    for (var j = 0; j < feathers; j++) {
+        bone_geometry.vertices.push(curve.getPoint(j / feathers));
+    }
+    bone = new THREE.Line(bone_geometry, boneMaterial);
+    framework.scene.add(bone);
+    console.log(bone_geometry.vertices.length);
+    for (var i = 0; i < bone_geometry.vertices.length; i++) {
+        var vertex = bone_geometry.vertices[i];
+        var feather = framework.scene.getObjectByName(i);
+        if (feather) {
+            feather.position.set(vertex.x, vertex.y, vertex.z);
+        }
+    }
+
     for (var i = 0; i < visible_feathers_scale.feather_count; i++) {
         var feather = framework.scene.getObjectByName(i);
         var date = new Date();
+        var size_factor = feather_size_scale.feather_size;
         if (feather !== undefined) {
+            feather.scale.set((size_factor * i/feathers), (size_factor * i/feathers), (size_factor * i/feathers));
             feather.material.color.set(base_color + feather_color_scale.color * 167772);
-
             if (wind_direction_scale.wind_direction == 0) {
-                feather.rotateZ(wind_speed_scale.wind_speed / 100 * Math.sin(i * flap_motion + date.getTime() / 100) * 2 * Math.PI / 180);
+                feather.rotateZ(wind_speed_scale.wind_speed / 100 * Math.sin(i + date.getTime() / 100) * 2 * Math.PI / 180);
             }
             else {
-                feather.rotateY(wind_speed_scale.wind_speed / 100 * Math.cos(i * flap_motion + date.getTime() / 100) * 2 * Math.PI / 180);
+                feather.rotateY(wind_speed_scale.wind_speed / 100 * Math.cos(i + date.getTime() / 100) * 2 * Math.PI / 180);
             }
         }
     }
